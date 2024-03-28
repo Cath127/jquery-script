b791473be61284ddd04413e26be4e7862bcb6d4d
Catherine  Hull <cathh127@hotmail.com>
2024-03-28 07:50:04 +0000

    jQuery added

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("h2").css("text-decoration" "underline"); //underlines all <h2> elements
+    $("ul").css("border", "solid 1px #ccc"); //adds a border to all lists
+    $("h2").addClass("underline"); // adds a class
+    $("ul").removeClass("border"); // removes a class
+});
